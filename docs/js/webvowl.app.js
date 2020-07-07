8892093a9d91f34e595b1997d251e593e26711dc
eugeniogz <eugeniog@gmail.com>
2020-07-07 11:41:37 -0300

    \ZZ\

--- docs/js/webvowl.app.js
+++ docs/js/webvowl.app.js
@@ -8243,7 +8243,7 @@ webvowl.app =
 	  var visibilityStatus = true;
 	  var DEFAULT_JSON_NAME="./gbd/base_revisao";
 	  var newJSON = location.search.split('file=')[1]; // This file is loaded by default
-	  if (typeof newJSON != undefined && newJSON !=null ) {
+	  if (typeof newJSON != 'undefined' && newJSON !=null ) {
 		  DEFAULT_JSON_NAME =  "./gbd/" + newJSON;
 	  }
 	  
