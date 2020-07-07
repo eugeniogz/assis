f3482c7959ce1173481f711d6b5af95dc30b63d9
eugeniogz <eugeniog@gmail.com>
2020-07-07 11:29:46 -0300

    base_revisao

--- docs/js/webvowl.app.js
+++ docs/js/webvowl.app.js
@@ -8241,11 +8241,11 @@ webvowl.app =
 	  var missingImportsWarning = false;
 	  var showLoadingDetails = false;
 	  var visibilityStatus = true;
-	  
+	  var DEFAULT_JSON_NAME="./gbd/base_revisao";
 	  var DEFAULT_JSON_NAME = location.search.split('file=')[1]; // This file is loaded by default
-	  if (typeof(DEFAULT_JSON_NAME) == undefined) {
+	  if (DEFAULT_JSON_NAME == undefined) {
 		  DEFAULT_JSON_NAME="./gbd/base_revisao";
-	  } else { DEFAULT_JSON_NAME =  "./gbd/" + DEFAULT_JSON_NAME;}
+	  } e lse { DEFAULT_JSON_NAME =  "./gbd/" + DEFAULT_JSON_NAME;}
 	  
 	  var conversion_sessionId;
 	  
