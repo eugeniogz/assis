2df94bdd6f3921e93854ff09c3ca3b28710082fb
José Eugènio de Assis Gonçalves <eugeniog@gmail.com>
2024-05-04 16:39:21 -0300

    modificacoes ts, mas ainda não gera js ok

---
+++ src/test.ts
@@ -0,0 +1,3 @@
+export function getEnc() {
+  return "Hello World!";
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,27 @@
+//This is webpack.config.js
 const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
   // The entry point file described above
-  entry: './src/index.ts',
+  entry: './src/test.ts',
   // The location of the build folder described above
   output: {
     path: path.resolve(__dirname, 'docs/sjcllib'),
     filename: 'index.js'
   },
+  /*
   module: {
     rules: [
       {
         test: /\.ts$/,
         use: {
           loader: 'ts-loader',
-          options: {
-            transpileOnly: true,
-          },
         },
       },
     ],
   },
   resolve: {
     extensions: ['.ts', '.js'],
-  },
-  // Optional and for development only. This provides the ability to
-  // map the built code back to the original source format when debugging.
-  devtool: 'eval-source-map',
+  },*/
 };
