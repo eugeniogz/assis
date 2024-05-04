cfd5cbd9bdbcb83bf5daa3de74b9589d739e2834
José Eugènio de Assis Gonçalves <eugeniog@gmail.com>
2024-05-04 15:01:00 -0300

    transpile

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   // The entry point file described above
@@ -7,6 +8,22 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'docs/sjcllib'),
     filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
   },
   // Optional and for development only. This provides the ability to
   // map the built code back to the original source format when debugging.
