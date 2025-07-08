a60b1191dee7c98fac7901cc85f3a981c13fb4ee
José Eugênio <eugeniog@gmail.com>
2025-07-08 11:39:38 -0300

    fim tentativa salvar senha

--- docs/pwa.sjcl/app.js
+++ docs/pwa.sjcl/app.js
@@ -6,9 +6,9 @@ const statusMessage = document.getElementById('statusMessage');
 
 let fileHandle = null; // Armazena a referência ao arquivo
 
-document.getElementById('form').addEventListener('submit', function(event) {
-        event.preventDefault();
-});
+// document.getElementById('form').addEventListener('submit', function(event) {
+//         event.preventDefault();
+// });
 
 // Função para exibir mensagens de status
 function showStatus(message, isError = false) {
